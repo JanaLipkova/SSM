c8e8d91b3d37fe07e3a0685cc5d1e1845849c7e6
Jana Lipkova <jana.lipkova@tum.de>
2018-01-11 12:18:51 +0100

    add example for trajectories submission

---
+++ Simulations/Dimerization/Stiff/DimerisationStiffTrajAdaptiveS/submitAll.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+MethodsList="AdaptiveS"
+#MethodsList="SLeaping RLeapingJana TauLeaping"
+#MethodsList="AdaptiveS AdaptiveTau SLeaping TauLeaping RLeapingJana"
+EpsList="0.05 0.03 0.01"
+myBase=${PWD}
+echo My base is: ${myBase}
+
+for method in $MethodsList; do
+for eps in $EpsList; do
+
+methodFolder=${method}/eps_${eps}/
+
+cp ssm ${methodFolder}/bin
+#cp run_lrz.sh ${methodFolder}
+
+cd ${methodFolder}
+sbatch runHybridJob_lrz.sh
+
+cd ${myBase}
+done 
+done
