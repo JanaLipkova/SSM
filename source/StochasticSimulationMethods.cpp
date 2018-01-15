2b663bd5c1763a535eb937fea6c417fb6f1c7e50
Jana Lipkova <jana.lipkova@tum.de>
2018-01-15 15:10:40 +0100

    update computation of the time step tau in the S and Adaptive S, where time step is recomputed by Gamma distribution

--- source/StochasticSimulationMethods.cpp
+++ source/StochasticSimulationMethods.cpp
@@ -135,8 +135,8 @@ int main (int argc, char * const argv[])
 	{ method	= new RLeapingJana (simulation); }
 	 else if ( ( selectedMethod == "SLeap")		|| ( selectedMethod == "SLeaping")			|| ( selectedMethod == "S-Leap")		|| ( selectedMethod == "S-Leaping") )
 	 { method	= new SLeaping (simulation); }
-	//else if ( ( selectedMethod == "AdaptiveTauLeap")|| ( selectedMethod == "AdaptiveTauLeaping")|| ( selectedMethod == "AdaptiveTau")|| ( selectedMethod == "AdatpiveTau-Leaping") )
-	//{ method	= new AdaptiveTau (simulation); }
+	else if ( ( selectedMethod == "AdaptiveTauLeap")|| ( selectedMethod == "AdaptiveTauLeaping")|| ( selectedMethod == "AdaptiveTau")|| ( selectedMethod == "AdatpiveTau-Leaping") )
+	{ method	= new AdaptiveTau (simulation); }
 	else if ( ( selectedMethod == "AdaptiveSLeap")|| ( selectedMethod == "AdaptiveSLeaping")|| ( selectedMethod == "AdaptiveS")|| ( selectedMethod == "AdatpiveS-Leaping") )
 	{ method	= new AdaptiveSLeaping (simulation); }
 	// //else if ( ( selectedMethod == "AdaptiveSLeapCL")|| ( selectedMethod == "AdaptiveSLeapingCL")|| ( selectedMethod == "AdaptiveSCL")|| ( selectedMethod == "AdatpiveS-LeapingCL") )
