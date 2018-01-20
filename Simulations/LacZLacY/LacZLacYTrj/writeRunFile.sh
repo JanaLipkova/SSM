ca93f9838e8e06c6680be7677956b00875e42d2d
Georgios Arampatzis <garampat@eu-login-08.euler.ethz.ch>
2018-01-20 23:38:51 +0100

    small change in a script

--- Simulations/LacZLacY/LacZLacYTrj/writeRunFile.sh
+++ Simulations/LacZLacY/LacZLacYTrj/writeRunFile.sh
@@ -8,7 +8,15 @@ cat > ${scriptName} << EOF
 #! /bin/bash -f
 module load gcc boost
 ./ssm ${XMLFile} \$1 \$2
-rm ssm output run.sh LacZLacY-SSA-0.01.xml lacy_lacz2_histogram.txt
+rm ssm output run.sh 
+files=`ls -1 ./*.xml`
+for f in $files; do
+	rm $f
+done
+files=`ls -1 ./*histogram.txt`
+for f in $files; do
+	rm $f
+done
 EOF
 
 chmod 777 ${scriptName}
